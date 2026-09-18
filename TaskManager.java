e59754d9633cfceb2fd0754577505f221fe62b95
Fatimah <fatmh.md05@gmail.com>
2026-09-18 21:15:15 +0300

    the first step of the project

---
+++ TaskManager.java
@@ -0,0 +1,7 @@
+public class TaskManager {
+    public static void main(String[] args) {
+        System.out.println("=== My Tasks ===");
+        System.out.println("1. Finish SWQA assignment");
+        System.out.println("2. Study for SWC");
+    }
+}

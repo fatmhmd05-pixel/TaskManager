479c80ba3a4d14ace527ec70eb8f9a8584069be0
Fatimah <fatmh.md05@gmail.com>
2026-09-19 19:29:18 +0300

    new feature

--- TaskManager.java
+++ TaskManager.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class TaskManager {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
         System.out.println("=== My Tasks ===");
-        System.out.println("1. Finish SWQA assignment");
-        System.out.println("2. Study for SWC");
+        System.out.println("1. Finish Java assignment");
+        System.out.println("2. Study Git");
+
+        System.out.print("Enter a new task: ");
+        String newTask = input.nextLine();
+
+        System.out.println("3. " + newTask);
     }
 }
